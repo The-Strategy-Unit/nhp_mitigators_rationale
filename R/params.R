6b60631fdbd928302d52e0bec1e6f0dc8301c9dd
Matt Dray <18232097+matt-dray@users.noreply.github.com>
2025-01-07 20:41:46 +0000

    Fetch support data from Azure, handle daycases

--- R/params.R
+++ R/params.R
@@ -25,16 +25,9 @@ extract_params <- function(
         peer,
         "_", stringr::str_sub(baseline_year, 3, 4),
         "_", stringr::str_sub(horizon_year, 3, 4)
-      ),
-      strategy= dplyr::case_match(
-        strategy,
-        "bads_daycase" ~ "day_procedures_usually_dc",
-        "bads_daycase_occasional" ~ "day_procedures_occasionally_dc",
-        "bads_outpatients" ~ "day_procedures_usually_op",
-        "bads_outpatients_or_daycase" ~ "day_procedures_occasionally_op",
-        .default = strategy
       )
     ) |>
+    correct_day_procedures() |>
     dplyr::left_join(runs_meta, by = dplyr::join_by("peer" == "dataset")) |>
     dplyr::left_join(
       mitigator_lookup,
@@ -65,6 +58,71 @@ extract_params <- function(
 
 }
 
+# Rename or remove older versions of daycase mitigators if they exist
+correct_day_procedures <- function(x) {
+
+  # Identify pairs of bads/day_procedures mitigators with flag
+  flagged <- x |>
+    dplyr::mutate(
+      mitigator_code_flag = dplyr::case_when(
+        stringr::str_detect(
+          strategy,
+          "^bads_daycase$|^day_procedures_usually_dc$"  # old name/new name
+        ) ~ "IP-EF-005",  # might as well flag with the mitigator code
+        stringr::str_detect(
+          strategy,
+          "^bads_daycase_occasional$|^day_procedures_occasionally_dc$"
+        ) ~ "IP-EF-006",
+        stringr::str_detect(
+          strategy,
+          "^bads_outpatients$|^day_procedures_usually_op$"
+        ) ~ "IP-EF-007",
+        stringr::str_detect(
+          strategy,
+          "^bads_outpatients_or_daycase$|^day_procedures_occasionally_op$"
+        ) ~ "IP-EF-008",
+        .default = NA_character_
+      )
+    )
+
+  # Identify where a peer has more than one instance of the code, i.e. the
+  # mitigator is represented by both a bads and a day_procedures version. We'll
+  # use this info to filter out the bads version.
+  dupes <- flagged |>
+    dplyr::count(peer, mitigator_code_flag) |>
+    tidyr::drop_na(mitigator_code_flag) |>
+    dplyr::filter(n > 1)
+
+  # Remove bads mitigators if there's a day_procedures replacement for it
+  if (nrow(dupes) > 0) {
+    for (i in seq(nrow(dupes))) {
+      flagged <- flagged |>
+        dplyr::filter(
+          !(peer == dupes[[i, "peer"]] &
+              mitigator_code_flag == dupes[[i, "mitigator_code_flag"]] &
+              stringr::str_detect(strategy, "^bads_"))
+        )
+    }
+  }
+
+  # Remaining bads mitigators clearly don't have a replacement day_procedures
+  # version so we can just rename these ones.
+  flagged |>
+    dplyr::mutate(
+      strategy = dplyr::case_match(
+        strategy,
+        "bads_daycase" ~ "day_procedures_usually_dc",
+        "bads_daycase_occasional" ~ "day_procedures_occasionally_dc",
+        "bads_outpatients" ~ "day_procedures_usually_op",
+        "bads_outpatients_or_daycase" ~ "day_procedures_occasionally_op",
+        .default = strategy
+      )
+    ) |>
+    dplyr::select(-mitigator_code_flag)  # remove helper column
+
+}
+
+# Generate table of results
 report_params_table <- function(
     p,  # a single scheme's params
     parameter = c("activity_avoidance", "efficiencies")
@@ -75,8 +133,8 @@ report_params_table <- function(
   time_profiles <- p[["time_profile_mappings"]][[parameter]] |>
     purrr::map(unlist) |>
     purrr::map(tibble::enframe, "strategy", "time_profile") |>
-    data.table::rbindlist(idcol = "activity_type") |>
-    tibble::tibble()
+    purrr::list_rbind(names_to = "activity_type") |>
+    dplyr::tibble()
 
   parameter_data |>
     purrr::map_depth(2, "interval") |>
