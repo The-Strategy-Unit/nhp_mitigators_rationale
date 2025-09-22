db3ccdb37e5901ebec17600878da30112463c86c
Matt Dray <18232097+matt-dray@users.noreply.github.com>
2025-09-22 17:45:42 +0100

    Add env vars to deployment script

--- deploy.R
+++ deploy.R
@@ -4,6 +4,11 @@ rsconnect::deployDoc(
   doc = "index.qmd",
   appName = "nhp_mitigators_rationale",
   appTitle = "NHP mitigator selections and rationale",
+  envVars = c(
+    "AZ_STORAGE_EP",
+    "AZ_STORAGE_CONTAINER_RESULTS",
+    "AZ_STORAGE_CONTAINER_SUPPORT"
+  ),
   lint = FALSE,
   forceUpdate = TRUE
 )
