aa7151096330ba073cc00ab2c27d92788986f04f
Matt Dray <18232097+matt-dray@users.noreply.github.com>
2026-01-07 16:02:38 +0000

    Change app name and title in deploy

--- deploy.R
+++ deploy.R
@@ -2,8 +2,8 @@ rsconnect::deployDoc(
   server = "connect.strategyunitwm.nhs.uk",
   appId = 187,
   doc = "index.qmd",
-  appName = "nhp_mitigators_rationale",
-  appTitle = "NHP mitigator selections and rationale",
+  appName = "nhp_tpma_rationale",
+  appTitle = "NHP: rationale for TPMA selections",
   envVars = c(
     "AZ_STORAGE_EP",
     "AZ_STORAGE_CONTAINER_RESULTS",
